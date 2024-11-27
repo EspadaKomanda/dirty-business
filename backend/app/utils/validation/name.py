adc04c0e3b78405a38c7b799607419e8b504f263
Papin Nikolai <yaicenuk1987@gmail.com>
2024-11-28 02:52:01 +0300

    Update name.py

--- backend/app/utils/validation/name.py
+++ backend/app/utils/validation/name.py
@@ -1,12 +1,11 @@
 """
-Validation for username.
+Validation for name.
 """
 from .standard import validate_regex
 
 def validate_name(field_name, required: bool = False):
     """
-    Validates that a username contains only Latin characters, numbers, and underscores.
-    The username must be between 3 and 18 characters long.
+    Validates name.
     """
     name_regex = r'.{1,100}$'
 
@@ -14,6 +13,5 @@ def validate_name(field_name, required: bool = False):
         field_name,
         name_regex,
         required,
-        "must be from 3 to 18 characters long and can consist of lower"
-        "and upper latin characters, numbers and underscores."
+        "name cannot be longer than 100 characters."
     )
