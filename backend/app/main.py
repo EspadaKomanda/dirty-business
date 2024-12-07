6a5fb3106353110c6f1637e7a93e715fc94b663d
Nikolai Papin <nikolai@weirdcat.ru>
2024-12-07 20:20:19 +0300

    Potential fix for CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ def main():
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[
-            "*"
+            "127.0.0.1:3000"
         ],
         allow_credentials=True,
         allow_methods=["*"],
